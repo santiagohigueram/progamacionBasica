4228c71749064dd48a23e1a67a146f24d6e9b189
santiagohigueram <shigueram@udistrital.edu.co>
2025-09-05 12:59:29 -0500

    Add files via upload

---
+++ corte_1/practica1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop 
+hola estoy practicando programacion.
+
+*/
+//esque quiero aprender a programar en c
+
+int main(int argc, char *argv[]) 
+{
+	printf("Hola mundo");
+	return 0;
+}
