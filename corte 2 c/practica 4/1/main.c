d390b88e32fb22e182c1bebdabc65ca4faedc1a3
santiagohigueram <shigueram@udistrital.edu.co>
2025-12-09 18:17:34 -0500

    Add files via upload

---
+++ corte 2 c/practica 4/1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+int main() {
+    int arreglo[SIZE];
+    int i;
+    
+    printf("Introduce %d numeros enteros:\n", SIZE);
+    
+    for(i = 0; i < SIZE; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &arreglo[i]);
+    }
+    
+    printf("\nLos numeros que introdujiste son:\n");
+    for(i = 0; i < SIZE; i++) {
+        printf("%d ", arreglo[i]);
+    }
+    
+    return 0;
+}
+
