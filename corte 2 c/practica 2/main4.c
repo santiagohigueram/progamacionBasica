79988edf2dcd7c7f600a6971114332ae355eb068
santiagohigueram <shigueram@udistrital.edu.co>
2025-12-09 18:04:02 -0500

    Add files via upload

---
+++ corte 2 c/practica 2/main4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char carName[] = "Ferrari";
+  int length = sizeof(carName) / sizeof(carName[0]);
+  int i;
+  
+  for (i = 0; i < length; ++i) {
+    printf("%c\n", carName[i]);
+  }
+
+  return 0;
+}
